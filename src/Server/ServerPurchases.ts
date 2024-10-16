f06f6900b1a739371054cfaf767608bb7bfa96bf
Antoine Dubé <antoine.dube@gmail.com>
2024-10-16 06:08:55 -0400

    Cheaper hacking servers

--- src/Server/ServerPurchases.ts
+++ src/Server/ServerPurchases.ts
@@ -30,14 +30,12 @@ export function getCloudServerCost(ram: number): number {
     return Infinity;
   }
 
-  const upg = Math.max(0, Math.log(sanitizedRam) / Math.log(2) - 6);
+  // const upg = Math.max(0, Math.log(sanitizedRam) / Math.log(2) - 6);
 
   return (
-    0.01 *
     sanitizedRam *
     ServerConstants.BaseCostFor1GBOfRamServer *
-    currentNodeMults.CloudServerCost *
-    Math.pow(currentNodeMults.CloudServerSoftcap, upg)
+    currentNodeMults.CloudServerCost
   );
 }
 
